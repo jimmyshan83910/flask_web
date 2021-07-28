15f2d64209cfb2bb06aee6f8afb3b7af58a2f28e
jimmyshan <jimmysideproject@gmail.com>
2021-07-29 05:23:53 +0800

    feat: program optimization, create new api and remove useless file/folder.
    optimization:
    1. use __init__.py instead of flask Blueprint tech.
    
    create new api:
    1. create search_place.py, place.py, test.py
    
    file/folder change content:
    1. remove main.py, meal.py, backend folder

--- flask_app/api_project/__init__.py
+++ flask_app/api_project/__init__.py
@@ -4,5 +4,5 @@ from flask import Flask
 app = Flask(__name__)
 
 
-from api_project import search_meal
+from api_project import search_place
 from api_project import region_weather
--- flask_app/api_project/search_place.py
+++ flask_app/api_project/search_place.py
@@ -9,7 +9,6 @@ import time
 import re
 
 
-
 try:
     dotenv_path = join(dirname(os.path.abspath(__file__)), './G_API_key.env')
     load_dotenv(dotenv_path, override=True)
@@ -34,25 +33,30 @@ def city_keyword_mapping(specific_addr, keyword, radius_meter):
     
     for addr in specific_addr:
         loc = city_mapping(addr)
-        places = gmaps_key.places_nearby(
-            keyword=keyword, location=loc, radius=radius_meter, language='zh-TW')
+        places = gmaps_key.places_nearby(keyword=keyword, location=loc, 
+                                         radius=radius_meter, language='zh-TW')
         
     while places:
         for place_item in places['results']:
             try:
-                if re.search(keyword, place_item['name']) and place_item['vicinity']:
+                if (re.search(keyword, place_item['name']) and
+                    place_item['vicinity']):
+                    # place_item['vicinity'] is the place address 
                     city_str = place_item['plus_code']['compound_code'][-3:]
                     place_results[place_item['name']] = city_str + place_item['vicinity']
                 
-                if re.search(keyword, place_item['name']) and not place_item['vicinity']:
-                    place_results[place_item['name']] = city_str + place_item['formatted_address']
+                if (re.search(keyword, place_item['name']) and not 
+                    place_item['vicinity']):
+                    # if place_item['vicinity'] not exist, use this conditional
+                    place_results[place_item['name']] = place_item['formatted_address']
             except:
                 print("Grab address error, exit the program!")
                 exit()
 
         if 'next_page_token' in places:
             time.sleep(2)
-            places = gmaps_key.places_nearby(page_token=places['next_page_token'])
+            places = gmaps_key.places_nearby(
+                page_token=places['next_page_token'])
         else:
             break
 
@@ -61,8 +65,8 @@ def city_keyword_mapping(specific_addr, keyword, radius_meter):
 @app.route('/meal', methods = ['POST'])
 def meal():
     '''
-    Find specific place with keyword and can exchange search radius. 
-    By the way, googlemaps api can't access too frequently, so search would .
+    Find specific place with keyword, also can exchange search radius. 
+    By the way, googlemaps api can't access too frequently, so search would.
 
     Url example: http://13.231.176.185:80/meal
     Methods: POST
