f3bdff9db3b541de625f8e20cd9f9aabcc44f6dc
jimmyshan <jimmysideproject@gmail.com>
2021-07-29 06:20:22 +0800

    still in development.

--- flask_app/cron_task/update_vari.py
+++ flask_app/cron_task/update_vari.py
@@ -6,7 +6,6 @@ import json
 data = rq.get(
     "https://www.cwb.gov.tw/Data/js/info/Info_Town.js?v=20200817")
 
-
 # for item in data:
 #     input(item)
 result = re.search(r'({.*)', data.text, re.S).group(0).rstrip(';')
